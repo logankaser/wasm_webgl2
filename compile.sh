492725a90c29a6b33cca75bfe414ecfaf4dd0bf6
Logan James Kaser <logankaser@fyrn.io>
2018-08-29 14:14:47 -0700

    Lab computer support

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker run \
-  --rm \
-  -v $(pwd):/src \
-  -u emscripten \
-  trzeci/emscripten \
-  make
+docker start wasm
+docker exec --user root -t wasm rm -rf ./*
+docker cp . wasm:/src
+docker exec --user root -t wasm make
+docker cp wasm:/src/index.wasm .
+docker cp wasm:/src/index.js .
