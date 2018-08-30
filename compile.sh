e814aa1625e2a3f2616678ae14b0f647f6d72e78
Logan James Kaser <logankaser@fyrn.io>
2018-08-29 19:15:41 -0700

    No more docker.

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker start wasm
-docker exec --user root -t wasm rm -rf ./*
-docker cp . wasm:/src
-docker exec --user root -t wasm make
-docker cp wasm:/src/index.wasm .
-docker cp wasm:/src/index.js .
---
+++ lab.sh
@@ -0,0 +1,2 @@
+mkdir -p /tmp/wasm_cache
+export EM_CACHE=/tmp/wasm_cache
