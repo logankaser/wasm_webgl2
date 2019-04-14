ac452a0d31aba1c4bd72393e3cf10dbe3bfa0bda
Logan James Kaser <logankaser@fyrn.io>
2019-04-13 21:34:06 -0700

    add flag

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker-machine ssh $machine sudo rm -rf /home/docker/build
 docker-machine ssh $machine mkdir /home/docker/build
 
 echo "Sending build data to server.."
-mkdir obj
+mkdir -p obj
 scp -i ~/.docker/machine/machines/$machine/id_rsa -pqr . docker@$(docker-machine ip $machine):/home/docker/build
 echo "------------------------------\n"
 
