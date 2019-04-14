ebde2fc8fe620c9f6e3f3cb45706704ef8647bef
Logan James Kaser <logankaser@fyrn.io>
2019-04-13 19:49:44 -0700

    Improved readability of building with docker

--- build.sh
+++ build.sh
@@ -3,10 +3,15 @@ machine=default
 docker-machine ssh $machine sudo rm -rf /home/docker/build
 docker-machine ssh $machine mkdir /home/docker/build
 
-scp -i ~/.docker/machine/machines/$machine/id_rsa -p -r . docker@$(docker-machine ip $machine):/home/docker/build
+echo "Sending build data to server.."
+scp -i ~/.docker/machine/machines/$machine/id_rsa -pqr . docker@$(docker-machine ip $machine):/home/docker/build
+echo "------------------------------\n"
 
-docker start wasm_build_server
-docker exec -ti wasm_build_server bash -c ". /root/emsdk/emsdk_env.sh; make -C /root/build"
-docker stop wasm_build_server
+docker start wasm_build_server > /dev/null
+docker exec -ti wasm_build_server bash -c ". /root/emsdk/emsdk_env.sh > /dev/null; make -C /root/build"
+docker stop wasm_build_server > /dev/null
 
-scp -i ~/.docker/machine/machines/$machine/id_rsa -p -r docker@$(docker-machine ip $machine):/home/docker/build/\* .
+echo "\n------------------------------"
+echo "Fetching result from server..."
+scp -i ~/.docker/machine/machines/$machine/id_rsa -pqr docker@$(docker-machine ip $machine):/home/docker/build/\* .
+echo "Done."
--- scripts/start_build_server.sh
+++ scripts/start_build_server.sh
@@ -2,4 +2,4 @@
 
 echo "Creating build server instance.."
 
-docker create -t --name wasm_build_server -v /home/docker/build:/root/build wasm_build
+docker create -t --name wasm_build_server -v /home/docker/build:/root/build wasm_build_server
